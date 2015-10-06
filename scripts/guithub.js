84bdc66b8dd88ad3a39113e381fda24f9b5bb595
Christopher Nascone <cnasc@users.noreply.github.com>
2015-10-06 15:01:35 -0400

    Losing the SVG, just going to use Pixi

--- scripts/guithub.js
+++ scripts/guithub.js
@@ -1,8 +1,3 @@
 // GuitHub, a guitar scale helper web app
 // It tells you what notes to play! (sort of)
 
-// playing with Snap SVG for a bit
-var s = Snap("#fretboard");
-
-// draw a circle, see if it works
-var circle = s.circle(150, 150, 10);
