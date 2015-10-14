3217ca72f779365316966122bd2c000d1fe5b019
Christopher Nascone <chris@nascone.net>
2015-10-14 15:16:30 -0400

    minor formatting

--- scripts/guithub.js
+++ scripts/guithub.js
@@ -17,7 +17,8 @@ document.getElementById("canvas").appendChild(renderer.view);
 var stage = new PIXI.Container();
 
 // the chromatic scale (all 12 notes in Western music)
-var chromatic = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+var chromatic = ['A', 'A#', 'B', 'C', 'C#', 'D',
+  'D#', 'E', 'F', 'F#', 'G', 'G#'];
 
 // Contains interval formulas to compute which notes are in a scale
 // Source: http://www.bandnotes.info/tidbits/scales/half-whl.htm
@@ -37,68 +38,68 @@ var fretboard = {
   numStrings: 6,
   width: Math.floor(renderer.width * 0.9),
   height: Math.floor(renderer.height * 0.6),
-  
+
   xPos: function () {
     "use strict";
     return Math.floor((renderer.width - fretboard.width) / 2);
   },
-  
+
   yPos: function () {
     "use strict";
     return Math.floor((renderer.height - fretboard.height) / 3);
   },
-  
+
   drawBackground: function () {
     "use strict";
     // instantiate board
     var board = new PIXI.Graphics();
     board.lineStyle(2, this.black, 1);
     board.beginFill(this.fretboardColor);
-    
+
     // draw the board
     board.drawRect(this.xPos(), this.yPos(), this.width, this.height);
     board.endFill();
     stage.addChild(board);
   },
-  
+
   drawFrets: function () {
     "use strict";
     var lineIncr, lineX, i, fret;
     // determine position and offset for frets
     lineIncr = Math.floor(this.width / this.numFrets);
     lineX = this.xPos() + lineIncr;
-    
+
     for (i = 0; i < this.numFrets - 1; i++) {
       fret = new PIXI.Graphics();
       fret.lineStyle(2, this.black, 1);
-      
+
       fret.moveTo(lineX, this.yPos());
       fret.lineTo(lineX, this.yPos() + this.height);
       stage.addChild(fret);
-      
+
       lineX += lineIncr;
     }
   },
-  
+
   drawStrings: function () {
     "use strict";
     var lineIncr, lineY, i, string;
     // determine position and offset for strings
     lineIncr = Math.floor(this.height / (this.numStrings - 1));
     lineY = this.yPos() + lineIncr;
-    
+
     for (i = 0; i < this.numStrings - 2; i++) {
       string = new PIXI.Graphics();
       string.lineStyle(1, this.black, 1);
-      
+
       string.moveTo(this.xPos(), lineY);
       string.lineTo(this.xPos() + this.width, lineY);
       stage.addChild(string);
-      
+
       lineY += lineIncr;
     }
   },
-  
+
   drawBoard: function () {
     "use strict";
     this.drawBackground();
@@ -107,11 +108,11 @@ var fretboard = {
   }
 };
 
-// returns an array of notes in the scale, or 
+// returns an array of notes in the scale, or
 function buildScale(root, type) {
   "use strict";
   var scale, formula, note;
-  
+
   switch (type) {
     case "major":
       formula = scales.major;
@@ -129,16 +130,16 @@ function buildScale(root, type) {
       // This should only happen if the user tampers with the HTML
       return null;
   }
-  
+
   scale = [];
   note = chromatic.indexOf(root);
   scale.push(note);
-  
+
   for (var i = 0; i < formula.length; i++) {
     note = (note + formula[i]) % chromatic.length;
     scale.push(note);
   }
-  
+
   return scale;
 }
 
