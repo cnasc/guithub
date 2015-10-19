aebf7b1c15fb63ca9092078e61bfc8a0bae17413
Christopher Nascone <chris@nascone.net>
2015-10-19 13:42:58 -0400

    License clarity

--- scripts/guithub.js
+++ scripts/guithub.js
@@ -1,6 +1,20 @@
-// GuitHub, a guitar scale helper web app
-// It tells you what notes to play! (sort of)
+/*
+    Copyright 2015 Christopher Nascone
+    This file is part of GuitHub.
 
+    GuitHub is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    GuitHub is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GuitHub.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /*global PIXI */
 
 // Create the renderer
